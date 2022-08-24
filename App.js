1924c0ed943207926b2c7824df87f4ed5660b025
Ali Ahmad <mian41935@gmail.com>
2022-08-24 23:32:39 +0500

    Express server Initialization

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
 const exppres = require('express');
+const http = require('http');
+
+const app = exppres();
+//Server Port
+const PORT = process.env.PORT || 3000;
+
+// http server
+const server = http.createServer(app);
+app.use(exppres.static('public'));
+
+// Server Route Create Append Html File
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// listen Server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
