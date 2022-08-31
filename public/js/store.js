ba84f5919d3cf9de04d842c52e7614c1ea952e27
Ali Ahmad <mian41935@gmail.com>
2022-08-31 21:59:32 +0500

    State management add custom store

---
+++ public/js/store.js
@@ -0,0 +1,53 @@
+let state = {
+  socketId: null,
+  localStream: null,
+  remoteStream: null,
+  screenSharingStream: null,
+  allowConnectionFromStrangers: false,
+  screenSharingActive: false,
+};
+export const setSocketId = (socketId) => {
+  state = {
+    ...state,
+    socketId,
+  };
+  console.log(state);
+};
+
+export const setLocalStream = (stream) => {
+  state = {
+    ...state,
+    localStream: stream,
+  };
+};
+
+export const allowConnectionFromStrangers = (allowConnection) => {
+  state = {
+    ...state,
+    allowConnectionFromStrangers: allowConnection,
+  };
+};
+
+export const screenSharingActive = (screenSharingActive) => {
+  state = {
+    ...state,
+    screenSharingActive,
+  };
+};
+export const setScreenSharingStream = (stream) => {
+  state = {
+    ...state,
+    screenSharingStream: stream,
+  };
+};
+export const setRemoteStream = (stream) => {
+  state = {
+    ...state,
+    remoteStream: stream,
+  };
+};
+ 
+export const getState = () => {
+
+    return state;
+}
